80052c5296ea188f74672b9802543348e86d93a3
ddonniee <mj940103@hanmail.net>
2022-01-09 21:36:20 +0900

    calendar pure ui

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 /* 코드를 작성해 보세요. */
+import Calendar from 'tui-calendar';
+import 'tui-calendar/dist/tui-calendar.css'
+import 'tui-date-picker/dist/tui-date-picker.css';
+import 'tui-time-picker/dist/tui-time-picker';
+
+const calendar = new Calendar('#calendar', {
+    defaultView: 'month',
+    useCreationPopup: true,
+    useDetailPopup: true,
+    // taskView 속성은 마일스톤/업무 보기를 설정ㅎ하여 일간, 주간 보기에서만 지원되는 기능으로 기본값을 false로 해두자.
+    taskView: false,
+    scheduleView: false,
+    month: {
+        workweek: true
+    },
+    week: {
+        workweek: true
+    }
+})
+
+
+
 
 /* ---------------------------------------------- */
 /* 이동 및 뷰 타입 변경 버튼 이벤트 핸들러 */
+
+// 이전, 다음, 일간, 주간, 월간 버튼 관리
+// 상세페이지 연결은 해당 일자 클릭했을 때
+
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 const dayViewBtn = document.getElementById('dayViewBtn');
@@ -27,3 +53,5 @@ weekViewBtn.addEventListener('click', () => {
 monthViewBtn.addEventListener('click', () => {
 
 });
+
+calendar.render();
